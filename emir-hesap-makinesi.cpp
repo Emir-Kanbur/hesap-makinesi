8d7dacba2b9fbb33be602b79198d757ca03eb770
Emir-Kanbur <16008125024@ogr.bozok.edu.tr>
2025-10-19 17:47:09 +0300

    ..

---
+++ emir-hesap-makinesi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int sayi1 , sayi2;
+
+cout << "Lütfen iki adet sayı giriniz:"<<endl;
+
+cout<< "Lütfen birinci sayıyı giriniz:"<<endl;
+
+cin>>sayi1;
+
+cout<<"Sayınız:"<<sayi1<<endl;
+
+cout<<"lütfen ikinci sayıyı giriniz:"<<endl;
+
+cin>>sayi2;
+
+cout<<"Sayınız:"<<sayi2<<endl;
+
+cout<<"Toplam:"<<sayi1+sayi2<<endl;
+cout<<"Kalan:"<<sayi1-sayi2<<endl;
+cout<<"Çarpım:"<<sayi1*sayi2<<endl;
+cout<<"Bölüm:"<<sayi1/sayi2<<endl;
+
+}
+
+
