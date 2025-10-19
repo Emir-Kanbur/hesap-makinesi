1fc7dd6e56ea630208ebfba5d931d6bf8f243381
harunyobu <16008125089@ogr.bozok.edu.tr>
2025-10-19 17:47:06 +0300

    Update temel-hesap-makinesi.cpp

---
+++ temel-hesap-makinesi.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; 
+    cout << "Yapmak istediğiniz İşlemi Seçiniz: "; 
+    cout << "\n1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme\n"; 
+    cin >> x; 
+    if (x == 1) {
+        int y;
+        int z;
+        cout << "İlk sayıyı giriniz ";
+        cin >> y;
+        cout << "İkinci sayıyı giriniz ";
+        cin >> z;
+        cout << y + z;
+        
+    }
+    else if (x == 2) {
+        int y;
+        int z;
+        cout << "İlk sayıyı giriniz ";
+        cin >> y;
+        cout << "İkinci sayıyı giriniz ";
+        cin >> z;
+        cout << y - z;
+        
+    }
+    else if (x == 3) {
+        int y;
+        int z;
+        cout << "İlk sayıyı giriniz ";
+        cin >> y;
+        cout << "İkinci sayıyı giriniz ";
+        cin >> z;
+        cout << y * z;
+        
+    }
+    else if (x == 4) {
+        int y;
+        int z;
+        cout << "İlk sayıyı giriniz ";
+        cin >> y;
+        cout << "İkinci sayıyı giriniz ";
+        cin >> z;
+        cout << y / z;
+        
+    }
+
+    return 0;
+}
