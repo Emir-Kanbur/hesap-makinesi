6bd067b4bfe2244c50c3088abe1274332e18a875
16008125085-blip <16008125085@ogr.bozok.edu.tr>
2025-10-19 17:30:08 +0300

    ...

--- temel-hesap-makinesi.cpp
+++ temel-hesap-makinesi.cpp
@@ -7,3 +7,12 @@ cout<<"Sayınız:"<<sayi1<<endl;
 cout<<"lütfen ikinci sayıyı giriniz:"<<endl;
 
 cin>>sayi2;
+
+cout<<"Sayınız:"<<sayi2<<endl;
+
+cout<<"Toplam:"<<sayi1+sayi2<<endl;
+cout<<"Kalan:"<<sayi1-sayi2<<endl;
+cout<<"Çarpım:"<<sayi1*sayi2<<endl;
+cout<<"Bölüm:"<<sayi1/sayi2<<endl;
+
+}
