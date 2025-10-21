61d2c1ebfa8d20de61c56cdba20f749413d61288
16008125085-blip <16008125085@ogr.bozok.edu.tr>
2025-10-21 20:06:23 +0300

    .

--- detay-hesap-makinesi.cpp
+++ detay-hesap-makinesi.cpp
@@ -30,4 +30,34 @@ int main() {
         } else {
             double result = sqrt(number);
             cout << "Sonuç: " << result;
-        }}
+        }
+        
+     }
+    else if (x == 3) {
+        int a, b;
+        cout << "Birinci sayıyı giriniz: ";
+        cin >> a;
+        cout << "İkinci sayıyı giriniz: ";
+        cin >> b;
+        int result = a % b;
+        cout << "Sonuç: " << result;
+        
+    }
+    else if (x == 4) {
+        int n;
+        cout << "Faktöriyelini almak istediğiniz sayıyı giriniz: ";
+        cin >> n;
+        if(n < 0) {
+            cout << "Negatif sayının faktöriyeli alınamaz.";
+        } else {
+            long long result = 1;
+            for(int i = 1; i <= n; i++) {
+                result *= i;
+            }
+            cout << "Sonuç: " << result;
+        }
+        
+    }
+
+    return 0;
+}
